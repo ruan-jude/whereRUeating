f113ddc4ab4eea9ba51106246343e54b4fd286b0
Ruan Jude Angelo Jamolod <rtj36@cs431-00.cs.rutgers.edu>
2022-10-12 22:41:19 -0400

    Added database connection functionality, able to add dish names into database

--- server/AccountServices.py
+++ server/AccountServices.py
@@ -1,38 +1,39 @@
-import bcrypt
 import sys
+import bcrypt
 import mariadb
 
 #i have no idea if any of this works
 
 def setup_cursor(mode):
-    db_user = ""
-    db_pwd = ""
+    db_user = "root"
+    db_pwd = "VARRC"
 
+    '''
     if mode == "read":
         db_user = ""
         db_pwd = ""
     elif mode == "write":
         db_user = ""
         db_pwd = ""
-    
+    '''
+
+    # connects to MariaDB through the root
     try:
         conn = mariadb.connect(
-            user = db_user,
-            password = db_pwd,
-            #host="",
-            port = 3306,
-            database = "testDB"
-        )
+                user = db_user,
+                password = db_pwd,
+                port = 3306,
+                database = "testDB"
+                )
         conn.autocommit = False
-        
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
-    
+
     cursor = conn.cursor()
     if mode == "write":
-        return cursor,conn
+        return cursor, conn
     else:
-        return cursor
+        return cursor, None
 
 #THIS FUNCTION CURRENTLY DOES NOT WORK
 def verify_password(username_input, password_input):
@@ -51,36 +52,40 @@ def verify_password(username_input, password_input):
     retrieved_hash = result_set.first[0]
     retrieved_salt = retrieved_hash[7:29] #.decode('utf-8')
     password_hash = retrieved_hash[29:]
-    
+
     new_hash = bcrypt.hashpw(password_input.encode('utf-8'), retrieved_salt)
     if retrieved_hash == new_hash:
         print('Valid login')
     else:
-        print('incorrect login')
-         
+        print('Incorrect login')
+
 def create_account(email_input, username_input, password_input):
-    cursor,write_conn = setup_cursor("write")
-    cursor.execute("SELECT email FROM users WHERE email=?", (email_input,))#database call that searches if someone already has that login
+    cursor, write_conn = setup_cursor("write")
+
+    # database call that searches if someone already has that login
+    cursor.execute("SELECT email FROM users WHERE email=?", (email_input,))
     if cursor.rowcount != 0:
-        print('account with that email already exists')
-        
-    sanitize_info(email_input, username_input)
-    
+        print('ERROR: Account with that email already exists')
+        return
+
+    # makes sure username and password are legal
+    sanitize_info(email_input, username_input, password_input)
+
+    # safely encrypts password
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(password_input.encode('utf-8'), salt)
-    
+
     cursor.execute("INSERT INTO users (email, username, password) VALUES (?,?,?)", (email_input, username_input, hash))
-    #cursor.commit()
     write_conn.commit()
-    
-def sanitize_info(email_input, username_input):
+
+def sanitize_info(email_input, username_input, password_inpout):
     #check length
     #check for disallowed characters
     print("will finish this later")
 
 
 def main():
-    create_account("testemail@gmail.com", "testUser", "testpassword")
+    create_account("testemail1@gmail.com", "testUser1", "testpassword")
     #verify_password("testUser", "testpassword") 
 
 if __name__ == "__main__":
--- server/MenuScraper.py
+++ server/MenuScraper.py
@@ -1,7 +1,8 @@
+import mariadb
 import requests
-from bs4 import BeautifulSoup as bs
 import pandas as pd
 from tabulate import tabulate
+from bs4 import BeautifulSoup as bs
 
 LiviMenuURL = ('Livingston', 'http://menuportal.dining.rutgers.edu/FoodPro/pickmenu.asp?sName=Rutgers+University+Dining&locationNum=03&locationName=Livingston+Dining+Commons&naFlag=1')
 BuschMenuURL = ('Busch', 'http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=Rutgers+University+Dining&locationNum=04&locationName=Busch+Dining+Hall&naFlag=1')
@@ -10,41 +11,67 @@ CDMenuURL = ('Nielson', 'http://menuportal.dining.rutgers.edu/FoodPro/pickmenu.a
 CampusMenuURLs = [LiviMenuURL, BuschMenuURL, CAMenuURL, CDMenuURL]
 
 '''
+Sets up connection to database
+'''
+def setupCursor():
+    db_user = "root"
+    db_pwd = "VARRC"
+
+    # attempts connection to MariaDB through the root user
+    try:
+        conn = mariadb.connect(
+                user = db_user,
+                password = db_pwd,
+                port = 3306,
+                database = "whereRUeating"
+                )
+        conn.autocommit = False
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+
+    cursor = conn.cursor()
+    return cursor, conn
+
+'''
 Collects the weekly menu for the specified dining hall
 '''
 def getDiningHallInfo(diningHallURL: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> None:
     results = dict()
 
-    for campus in CampusMenuURLs: 
+    for campus in CampusMenuURLs:
         req = requests.get(campus[1])
         soup = bs(req.text, 'html.parser')
 
         # collecting date and URLs of the weekly menu
         dateURL = soup.find("select").findAll("option")
         for date in dateURL:
-            dateMenu = getDateMenus(URLprefix + date['value'], campus[0])
-            results[date.getText()] = pd.concat([results[date.getText()], dateMenu]) if date.getText() in results else dateMenu
+            dateMenuDict = _getDateMenus(URLprefix + date['value'], campus[0])
+            dateMenuDF = pd.DataFrame(dateMenuDict)
+            results[date.getText()] = pd.concat([results[date.getText()], dateMenuDF]) if date.getText() in results else dateMenuDF
 
+    return dateMenuDict
+'''
     for date in results:
         print('===== ' + date + ' =====')
         print(tabulate(results[date], headers = 'keys', tablefmt = 'psql'))
+    '''
 
 '''
 Scrapes through each tab to retrieve all information
 '''
-def getDateMenus(URL: str, campus: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> pd.DataFrame:
+def _getDateMenus(URL: str, campus: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> pd.DataFrame:
     req = requests.get(URL)
     soup = bs(req.text, 'html.parser')
 
     # collects all other tabs URLS
     tabURLs = dict()
     for t in soup.find_all('div', 'tab'):
-        if 'active' in t['class']: 
+        if 'active' in t['class']:
             tabURLs[t.getText()] = None
             continue
         tabURLs[t.getText()] = t.find('a')['href']
-    
-    # prints each meal's menu 
+
+    # prints each meal's menu
     prevLen = 0
     menuItems, servingSize, mealType = list(), list(), list()
     for t in tabURLs:
@@ -52,23 +79,54 @@ def getDateMenus(URL: str, campus: str, URLprefix: str = 'http://menuportal.dini
         req = requests.get(currURL)
         soup = bs(req.text, 'html.parser')
 
-        # collecting menu items and serving size   
+        # collecting menu items and serving size
         menuItems += [i.getText() for i in soup.find_all('div', 'col-1')]
         servingSize += [s.getText() for s in soup.find_all('div', 'col-2')]
         mealType += [t for _ in range(len(menuItems) - prevLen)]
         prevLen = len(menuItems)
-    
+
     campus = [campus for _ in range(prevLen)]
 
-    df = pd.DataFrame({
-        'Menu Item': menuItems,
-        'Serving Size' : servingSize,
-        'Meal Type': mealType,
-        'Campus': campus
-    })  
+    menuData = {
+            'Menu Item': menuItems,
+            'Serving Size' : servingSize,
+            'Meal Type': mealType,
+            'Campus': campus
+            }
 
-    return df
+    df = pd.DataFrame(menuData)
 
-getDiningHallInfo(CAMenuURL)
+    return menuData
+
+'''
+Adds dishes into the dishes table through MariaDB
+'''
+def addDishes(itemList):
+    cursor, write_conn = setupCursor()
+
+    for item in itemList:
+        # check if the item already exists
+        cursor.execute("SELECT name FROM dishes WHERE name=?", (item,))
+        if cursor.rowcount != 0:
+            print('ERROR: Dish already exists')
+            continue
+
+        # adds item into the table
+        cursor.execute("INSERT INTO dishes (name) VALUES (%s)", (item,))
+        write_conn.commit()
+
+        # gets the id of the item
+        #cursor.execute()
 
 
+
+def main():
+    #for ()
+
+    menuData = getDiningHallInfo(CAMenuURL)
+    #print(menuData['Campus'])
+    addDishes(menuData['Menu Item'])
+
+
+if __name__ == "__main__":
+    main()
