17478d865f1596657e7aaed552cce119418a109a
CHoang-public <32404336+CHoang-public@users.noreply.github.com>
2022-10-15 23:01:21 -0400

    create_account and verify_password scripts work

--- server/AccountServices.py
+++ server/AccountServices.py
@@ -1,92 +1,87 @@
-import sys
 import bcrypt
+import sys
 import mariadb
 
 #i have no idea if any of this works
 
 def setup_cursor(mode):
-    db_user = "root"
-    db_pwd = "VARRC"
-
-    '''
+    db_user = ""
+    db_pwd = ""
+   
     if mode == "read":
         db_user = ""
         db_pwd = ""
     elif mode == "write":
         db_user = ""
         db_pwd = ""
-    '''
-
-    # connects to MariaDB through the root
+    
     try:
         conn = mariadb.connect(
-                user = db_user,
-                password = db_pwd,
-                port = 3306,
-                database = "testDB"
-                )
+            user = db_user,
+            password = db_pwd,
+            #host="",
+            port = 3306,
+            database = "testDB"
+        )
         conn.autocommit = False
+        
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
-
+    
     cursor = conn.cursor()
-    if mode == "write":
-        return cursor, conn
-    else:
-        return cursor, None
+    return cursor,conn
+
 
 #THIS FUNCTION CURRENTLY DOES NOT WORK
 def verify_password(username_input, password_input):
-    cursor = setup_cursor("read")
-    print(username_input)
-    print(cursor)
-    cursor.execute("SELECT * FROM users")
+    cursor,read_conn = setup_cursor("read")
+    cursor.execute("SELECT password FROM users WHERE username=?", (username_input,))
     result_set = cursor.fetchall()  
- # result_set = cursor.execute("SELECT password FROM users WHERE username=?", (username_input,))
 
     if result_set == None:
         print("invalid_login: user does not exist")
         return
 
-    print(result_set)
-    retrieved_hash = result_set.first[0]
+    retrieved_hash = result_set[0][0]
     retrieved_salt = retrieved_hash[7:29] #.decode('utf-8')
     password_hash = retrieved_hash[29:]
 
-    new_hash = bcrypt.hashpw(password_input.encode('utf-8'), retrieved_salt)
-    if retrieved_hash == new_hash:
+    
+    password_matches = bcrypt.checkpw(password_input.encode('utf-8'), retrieved_hash.encode('utf-8'))
+    if password_matches:
         print('Valid login')
     else:
-        print('Incorrect login')
+        print('incorrect login')
 
+    read_conn.close()
+    
+         
 def create_account(email_input, username_input, password_input):
-    cursor, write_conn = setup_cursor("write")
-
-    # database call that searches if someone already has that login
-    cursor.execute("SELECT email FROM users WHERE email=?", (email_input,))
+    cursor,write_conn = setup_cursor("write")
+    cursor.execute("SELECT email FROM users WHERE email=?", (email_input,))#database call that searches if someone already has that login
     if cursor.rowcount != 0:
-        print('ERROR: Account with that email already exists')
-        return
-
-    # makes sure username and password are legal
-    sanitize_info(email_input, username_input, password_input)
-
-    # safely encrypts password
+        print('account with that email already exists')
+        
+    sanitize_info(email_input, username_input)
+    
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(password_input.encode('utf-8'), salt)
-
+    
     cursor.execute("INSERT INTO users (email, username, password) VALUES (?,?,?)", (email_input, username_input, hash))
+    #cursor.commit()
     write_conn.commit()
-
-def sanitize_info(email_input, username_input, password_inpout):
+    print("Created user or something")
+    write_conn.close()
+    
+def sanitize_info(email_input, username_input):
     #check length
     #check for disallowed characters
     print("will finish this later")
 
 
 def main():
-    create_account("testemail1@gmail.com", "testUser1", "testpassword")
-    #verify_password("testUser", "testpassword") 
+    #create_account("testemail@gmail.com", "testUser", "testpassword")
+    verify_password("testUser", "testpassword") 
 
 if __name__ == "__main__":
     main()
