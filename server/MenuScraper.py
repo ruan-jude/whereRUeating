cca50b5dedcfebd8e9fe21f0e4dd128eee4d75de
Ruan Jude Angelo Jamolod <rtj36@cs431-00.cs.rutgers.edu>
2022-10-19 14:52:39 -0400

    Menu collection entered into dishes and menuItems tables. Automation of script set up through Cron (waiting to test).

--- server/MenuScraper.py
+++ server/MenuScraper.py
@@ -1,8 +1,11 @@
 import mariadb
 import requests
+from datetime import datetime
+import calendar
 import pandas as pd
 from tabulate import tabulate
 from bs4 import BeautifulSoup as bs
+from AccountServices import *
 
 LiviMenuURL = ('Livingston', 'http://menuportal.dining.rutgers.edu/FoodPro/pickmenu.asp?sName=Rutgers+University+Dining&locationNum=03&locationName=Livingston+Dining+Commons&naFlag=1')
 BuschMenuURL = ('Busch', 'http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=Rutgers+University+Dining&locationNum=04&locationName=Busch+Dining+Hall&naFlag=1')
@@ -11,122 +14,123 @@ CDMenuURL = ('Nielson', 'http://menuportal.dining.rutgers.edu/FoodPro/pickmenu.a
 CampusMenuURLs = [LiviMenuURL, BuschMenuURL, CAMenuURL, CDMenuURL]
 
 '''
-Sets up connection to database
-'''
-def setupCursor():
-    db_user = "root"
-    db_pwd = "VARRC"
-
-    # attempts connection to MariaDB through the root user
-    try:
-        conn = mariadb.connect(
-                user = db_user,
-                password = db_pwd,
-                port = 3306,
-                database = "whereRUeating"
-                )
-        conn.autocommit = False
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-
-    cursor = conn.cursor()
-    return cursor, conn
-
-'''
 Collects the weekly menu for the specified dining hall
 '''
 def getDiningHallInfo(diningHallURL: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> None:
     results = dict()
+    diningHallNames = {'Livingston':'2', 'Busch':'3', 'Nielson':'4', 'Brower':'1'}
 
+	# iterates through each campus food site
     for campus in CampusMenuURLs:
         req = requests.get(campus[1])
         soup = bs(req.text, 'html.parser')
 
-        # collecting date and URLs of the weekly menu
-        dateURL = soup.find("select").findAll("option")
-        for date in dateURL:
-            dateMenuDict = _getDateMenus(URLprefix + date['value'], campus[0])
-            dateMenuDF = pd.DataFrame(dateMenuDict)
-            results[date.getText()] = pd.concat([results[date.getText()], dateMenuDF]) if date.getText() in results else dateMenuDF
+        # iterates through each day menu and collects them
+        menuDicts = dict()
+        dayURLs = soup.find("select").findAll("option")
+        for date in dayURLs:
+            dateStr = _dateToString(date.getText())
+            dateMenuDict = _getDateMenus(URLprefix + date['value'])
+            menuDicts[dateStr] = dateMenuDict
 
-    return dateMenuDict
+        # adds the dishes with all the information needed
+        _addCampusDishes(int(diningHallNames[campus[0]]), menuDicts)
+
 '''
-    for date in results:
-        print('===== ' + date + ' =====')
-        print(tabulate(results[date], headers = 'keys', tablefmt = 'psql'))
-    '''
+Updates dishes and menuItems tables
+'''
+def _addCampusDishes(diningHallInt:int, menuDicts:dict):
+    cursor, write_conn = setup_cursor("write")
+
+    for day in menuDicts:
+        menuDict = menuDicts[day]
+        dishes, meal = menuDict['Dishes'], menuDict['Meal']
+
+        for i, dish in enumerate(dishes):
+            # check if the item already exists
+            # if it doesn't exist, add it into dishes
+            cursor.execute("SELECT name FROM dishes WHERE name=?", (dish,))
+            if cursor.rowcount == 0:
+                cursor.execute("INSERT INTO dishes (name) VALUES (%s)", (dish,))
+                write_conn.commit()
+
+            # gets the index of current dish
+            cursor.execute("SELECT id FROM dishes WHERE name=?", (dish,))
+            res = cursor.fetchone()
+            id = res[0]
+
+            # add dish and date into menuItems table
+            cursor.execute("INSERT INTO menuItems (dish_id, restaurant_id, date, meal_time) VALUES (%d, %d, %s, %s)", (id, diningHallInt, day, meal[i]))
+            write_conn.commit()
+
+
+'''
+Returns the date in YYYY-MM-DD form
+'''
+def _dateToString(fullDate:str):
+    # removes the day of the week (will not store this)
+    fullDate = fullDate[fullDate.index(' ')+1:]
+
+    # gets the month integer
+    spaceInd = fullDate.index(' ')
+    monthInt = datetime.strptime(fullDate[:spaceInd], '%B').month
+    monthStr = '0'+str(monthInt) if monthInt < 10 else str(monthInt)	# pads beginning zero if needed
+
+    # gets the day
+    commaInd = fullDate.index(',')
+    dayStr = fullDate[spaceInd+1:commaInd]
+    dayStr = '0'+dayStr if int(dayStr) < 10 else dayStr		# pads beginning zero if needed
+
+    # gets the year
+    yearStr = fullDate[commaInd+2:]
+
+    return yearStr + '-' + monthStr + '-' + dayStr
 
 '''
 Scrapes through each tab to retrieve all information
 '''
-def _getDateMenus(URL: str, campus: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> pd.DataFrame:
+def _getDateMenus(URL: str, URLprefix: str = 'http://menuportal.dining.rutgers.edu/FoodPro/') -> pd.DataFrame:
     req = requests.get(URL)
     soup = bs(req.text, 'html.parser')
 
-    # collects all other tabs URLS
-    tabURLs = dict()
+    # collects all meal time URLs
+    mealURLs = dict()
     for t in soup.find_all('div', 'tab'):
         if 'active' in t['class']:
-            tabURLs[t.getText()] = None
+            mealURLs[t.getText()] = None
             continue
-        tabURLs[t.getText()] = t.find('a')['href']
+        mealURLs[t.getText()] = t.find('a')['href']
 
-    # prints each meal's menu
+    # collects each day's menu
     prevLen = 0
-    menuItems, servingSize, mealType = list(), list(), list()
-    for t in tabURLs:
-        currURL = URLprefix + tabURLs[t] if tabURLs[t] else URL
+    menuItems, mealType = list(), list()
+    for t in mealURLs:
+        currURL = URLprefix + mealURLs[t] if mealURLs[t] else URL
         req = requests.get(currURL)
         soup = bs(req.text, 'html.parser')
 
-        # collecting menu items and serving size
+        # collecting menu items and type
         menuItems += [i.getText() for i in soup.find_all('div', 'col-1')]
-        servingSize += [s.getText() for s in soup.find_all('div', 'col-2')]
         mealType += [t for _ in range(len(menuItems) - prevLen)]
-        prevLen = len(menuItems)
-
-    campus = [campus for _ in range(prevLen)]
 
     menuData = {
-            'Menu Item': menuItems,
-            'Serving Size' : servingSize,
-            'Meal Type': mealType,
-            'Campus': campus
+            'Dishes': menuItems,
+            'Meal': mealType
             }
-
-    df = pd.DataFrame(menuData)
 
     return menuData
 
 '''
-Adds dishes into the dishes table through MariaDB
+Deletes all items from menuItems table
 '''
-def addDishes(itemList):
-    cursor, write_conn = setupCursor()
-
-    for item in itemList:
-        # check if the item already exists
-        cursor.execute("SELECT name FROM dishes WHERE name=?", (item,))
-        if cursor.rowcount != 0:
-            print('ERROR: Dish already exists')
-            continue
-
-        # adds item into the table
-        cursor.execute("INSERT INTO dishes (name) VALUES (%s)", (item,))
-        write_conn.commit()
-
-        # gets the id of the item
-        #cursor.execute()
-
-
+def clearMenuItemsTable():
+    cursor, write_conn = setup_cursor("write")
+    cursor.execute("DELETE FROM menuItems")
+    write_conn.commit()
 
 def main():
-    #for ()
-
-    menuData = getDiningHallInfo(CAMenuURL)
-    #print(menuData['Campus'])
-    addDishes(menuData['Menu Item'])
-
+    clearMenuItemsTable()
+    getDiningHallInfo(CAMenuURL)
 
 if __name__ == "__main__":
     main()
